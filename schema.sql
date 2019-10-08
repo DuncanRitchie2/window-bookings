f84f7a5c069cec9a5b3b102c19df5533c217512b
Duncan Ritchie <duncanritchie@btopenworld.com>
2019-10-08 17:58:12 +0100

    Made windows & photos tables

--- schema.sql
+++ schema.sql
@@ -25,6 +25,7 @@ insert into customers(firstName, lastName, phoneNumber, emailAddress) values
 ;
 -- select * from customers;
 
+-- drop table premises;
 create table premises(
 	houseNumber int,
     houseName varchar(100),
@@ -44,14 +45,15 @@ insert into premises(houseName, street, town, country, postCode) values
 ;
 -- select * from premises;
 
+-- drop table surveys;
 create table surveys(
 	customer_id int,
     premises_id int,
     id int primary key auto_increment not null unique,
     dateToHappen datetime,
     dateBooked datetime default current_timestamp,
-	FOREIGN KEY(customer_id) REFERENCES customers(id),
-	FOREIGN KEY(premises_id) REFERENCES premises(id)
+	foreign key (customer_id) references customers(id),
+	foreign key (premises_id) references premises(id)
 );
 insert into surveys(customer_id, premises_id, dateToHappen) values
 	(1, 4, "2019-11-08 12:00:00"),
@@ -59,5 +61,35 @@ insert into surveys(customer_id, premises_id, dateToHappen) values
 	(4, 2, "2019-10-10 17:15:00"),
 	(11, 3, "2019-10-09 06:30:00")
 ;
-select * from surveys;
-select firstName, lastName, emailAddress, houseNumber, street, town, country, postCode from surveys join premises on surveys.premises_id = premises.id join customers on surveys.customer_id = customers.id;
+-- select * from surveys;
+-- select firstName, lastName, emailAddress, houseNumber, street, town, country, postCode from surveys join premises on surveys.premises_id = premises.id join customers on surveys.customer_id = customers.id;
+
+-- drop table windows;
+create table windows(
+	premises_id int,
+    description varchar(255),
+    id int primary key auto_increment not null unique,
+    foreign key(premises_id) references premises(id)
+);
+insert into windows(premises_id, description) values
+	(1, "Front window"),
+	(1, "Side window"),
+	(2, "Oval Office window"),
+	(2, "West Wing window");
+-- select * from windows;
+
+-- drop table photos;
+create table photos(
+    id int primary key auto_increment not null unique,
+	survey_id int,
+    window_id int,
+    url varchar(255),
+    dateTaken datetime default current_timestamp,
+    foreign key(survey_id) references surveys(id),
+    foreign key(window_id) references windows(id)
+);
+insert into photos(survey_id, window_id, url) values
+	(1, 1, "https://assets.publishing.service.gov.uk/government/assets/history/buildings/number-10-300-8ce94bd7c585b701c420d159f8c79f9b0a24b65860ef33fafcff92039c15870e.jpg"),
+    (3, 3, "https://live.staticflickr.com/3538/3484815800_04033e5049_b.jpg")
+;
+-- select * from photos join surveys on surveys.id = photos.survey_id join windows on windows.id = photos.window_id join premises on premises.id = windows.premises_id join customers on customers.id = surveys.customer_id;
